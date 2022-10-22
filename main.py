c490a56f640d9499072d745a86c4c5c0f107468b
JALLIFIER-TALMAT Ruben <ruben.jallifier@gmail.com>
2022-10-22 22:28:58 +0200

    insertion in associative table 'visit' ok

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def create_app():
 
     db.init_app(app)
 
-    def insert():
+    def insert_visit():
         con = db.get_db()
         cur = con.cursor()
         cur.execute(
-            "INSERT INTO patient (firstName, lastName, username, password, address, email, phone) VALUES ('mohamed', 'konate', 'moko', 'password', 'lyon', 'mohamed@mail.com', '0656980564')"
+            "INSERT INTO visit (id, id_1, 'appointment_time', 'cause') SELECT patient.id, doctor.id, '2003/01/22', 'foot hurt' FROM patient, doctor"
         )
         con.commit()
 
@@ -23,10 +23,28 @@ def create_app():
         db.close_db()
         return patients
 
+    def get_all_doctors():
+        con = db.get_db()
+        cur = con.cursor()
+        res = cur.execute("SELECT username, specialization FROM doctor")
+        doctors = res.fetchall()
+        db.close_db()
+        return doctors
+
+    def get_all_appointments():
+        con = db.get_db()
+        cur = con.cursor()
+        res = cur.execute("SELECT appointment_time, cause, id, id_1 FROM visit")
+        visits = res.fetchall()
+        db.close_db()
+        return visits
+
     @app.route("/home")
     def index():
-        #insert()
+        #insert_visit()
+        visits = get_all_appointments()
         patients = get_all_patients()
-        return render_template("home.html", patients=patients)
+        doctors = get_all_doctors()
+        return render_template("home.html", patients=patients, visits=visits, doctors=doctors)
 
     return app
