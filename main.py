ef3053c0b2058aebb6f8439e20e0d7bb85c015d7
JALLIFIER-TALMAT Ruben <ruben.jallifier@gmail.com>
2022-10-22 21:20:40 +0200

    display patients

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from ast import dump
 from flask import Flask, render_template
-
 
 def create_app():
     app = Flask(__name__)
@@ -13,26 +11,22 @@ def create_app():
         con = db.get_db()
         cur = con.cursor()
         cur.execute(
-            "INSERT INTO patient VALUES ('id', 'coucou', 'ruben', 'rub', 'password')"
+            "INSERT INTO patient (firstName, lastName, username, password, address, email, phone) VALUES ('mohamed', 'konate', 'moko', 'password', 'lyon', 'mohamed@mail.com', '0656980564')"
         )
         con.commit()
 
     def get_all_patients():
         con = db.get_db()
         cur = con.cursor()
-        res = cur.execute("SELECT firstName, lastName, username FROM patient")
-        lastName, firstName, username = res.fetchone()
+        res = cur.execute("SELECT firstName, lastName, email, phone FROM patient")
+        patients = res.fetchall()
         db.close_db()
-        return firstName, lastName, username
+        return patients
 
-   
-
-    @app.route("/")
+    @app.route("/home")
     def index():
-        firstName, lastName, username = get_all_patients()
-
-        return render_template(
-            "index.html", firstName=firstName, lastName=lastName, username=username
-        )
+        #insert()
+        patients = get_all_patients()
+        return render_template("home.html", patients=patients)
 
     return app
