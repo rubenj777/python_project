26cd52c9bc056335e24f01822e1772ed0584908e
JALLIFIER-TALMAT Ruben <ruben.jallifier@gmail.com>
2022-10-22 22:50:11 +0200

    super clean SQL request on associative table what best on saturday night

--- main.py
+++ main.py
@@ -11,9 +11,14 @@ def create_app():
         con = db.get_db()
         cur = con.cursor()
         cur.execute(
-            "INSERT INTO visit (id, id_1, 'appointment_time', 'cause') SELECT patient.id, doctor.id, '2003/01/22', 'foot hurt' FROM patient, doctor"
+            "INSERT INTO visit (id, id_1, 'appointment_time', 'cause') SELECT patient.id, doctor.id, '02/11/2022', 'foot hurt' FROM patient, doctor WHERE patient.id=2"
         )
         con.commit()
+
+    def get_id():
+        """cette fonction va servir à récupérer l'id du docteur connecté afin d'afficher seulement ses RDV dans la fonction get_all_appointments()"""
+        """"pour l'instant il n'y a pas de fonction de connexion alors elle retourne 1"""
+        return "1"
 
     def get_all_patients():
         con = db.get_db()
@@ -34,7 +39,7 @@ def create_app():
     def get_all_appointments():
         con = db.get_db()
         cur = con.cursor()
-        res = cur.execute("SELECT appointment_time, cause, id, id_1 FROM visit")
+        res = cur.execute("SELECT visit.appointment_time, visit.cause, visit.id, visit.id_1, patient.username FROM visit INNER JOIN patient ON visit.id = patient.id WHERE id_1=? ORDER BY appointment_time DESC", (get_id()))
         visits = res.fetchall()
         db.close_db()
         return visits
