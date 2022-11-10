2be9b79e86d390ece8b92829faae54249184e18c
JALLIFIER-TALMAT Ruben <ruben.jallifier@gmail.com>
2022-11-10 10:49:39 +0100

    insert doctor when init-db

--- database.sql
+++ database.sql
@@ -20,3 +20,5 @@ CREATE TABLE IF NOT EXISTS appointment(
    FOREIGN KEY(user_id) REFERENCES app_user(id),
    FOREIGN KEY(doc_id) REFERENCES app_user(id)
 );
+
+INSERT OR IGNORE INTO app_user ('firstName', 'lastName', 'username', 'password', 'address', 'email', 'phone', 'specialization', 'role') VALUES ('jean', 'michel', 'jm', 'mdp', 'lyon', 'jm@mail.com', '0660598045', 'chiropractor', '2');
